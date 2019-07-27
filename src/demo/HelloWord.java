3d469d31c809b9c6c3ecd8c7b8437d91e65e6d01
nijat <nijatmn@gmail.com>
2019-07-27 10:18:39 -0400

    Created a pause() method for freezing effect

--- src/demo/HelloWord.java
+++ src/demo/HelloWord.java
@@ -6,4 +6,14 @@ public class HelloWord {
 	public static void main(String[] args) {
 		System.out.println("Hello Students!");
 	}
+	
+	
+	// Method that pauses entire program for specified second
+	public static void pause(int second) {
+		try {
+			Thread.sleep(second * 1000); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
