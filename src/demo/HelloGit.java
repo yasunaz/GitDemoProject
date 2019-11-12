7f37bf7c4ca16003d1fc56ae738d2ca692e253a3
Guligeina <gabudusalamu@collabralink.com>
2019-11-12 00:29:15 -0500

    create new class that prasies git

---
+++ src/demo/HelloGit.java
@@ -0,0 +1,8 @@
+
+public class HelloGit {
+
+    public static void helloToGit() {
+        System.out.println("Git is the most amazing version control system");
+        System.out.println("It is distributed one too!!!");
+    }
+}
