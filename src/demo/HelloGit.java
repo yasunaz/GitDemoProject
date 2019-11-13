dad2a15dccaad78f445eccd8a4546473823191da
Nijat Muhtar <nijatmn@gmail.com>
2019-11-13 01:08:39 -0500

    added smile method

--- src/demo/HelloGit.java
+++ src/demo/HelloGit.java
@@ -5,4 +5,8 @@ public class HelloGit {
         System.out.println("Git is the most amazing version control system");
         System.out.println("It is distributed one too!!!");
     }
+
+    public void smile() {
+        System.out.println("smiling");
+    }
 }
